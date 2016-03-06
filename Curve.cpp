4615e3f4ce976133ed3780a55de50e3d14eb2342
Venny Larasati <venny_la@yahoo.com>
2016-03-06 11:38:47 +0700

    change Curve attributes & add functions

--- Curve.cpp
+++ Curve.cpp
@@ -1,11 +1,21 @@
 #include "Curve.h"
 
+
 Curve::Curve(){
 
 }
 
-Curve::Curve(Line, t){
+Curve::Curve(vector<Point> points, float d);{
+	float t=0;
+	pFinal.x = points.at(0).x;
+	pFinal.y = points.at(0).y;
+	finals.push_back(pFinal);
 
+	while (t<1){
+		quadraticBezier(t);
+		t+=d;
+		finals.push_back(pFinal);
+	}
 }
 
 Curve::Curve(const Curve& c){
@@ -17,5 +27,17 @@ Curve::Curve& operator=(const Curve& c){
 }
 
 Curve::~Curve(){
+	points.clear();
+	finals.clear();
+}
 
+void Curve::quadraticBezier(float t){ 
+	// Buat quadratic curve p0: source; p1: control; p2: destination
+	// Masih nggak ngerti caranya kalo curvenya lebih dr quadratic
+	pFinal.x = pow (1-t,2) * points.at(0).x +
+				(1-t) * 2 * t * points.at(1).x +
+				t * t * points.at(2).x;
+	pFinal.y = pow (1-t,2) * points.at(0).y +
+				(1-t) * 2 * t * points.at(1).y +
+				t * t * points.at(2).y;
 }
--- Curve.h
+++ Curve.h
@@ -3,19 +3,24 @@
 
 #include "Point.h"
 #include "Line.h"
+#include <math.h>
 
 using namespace std;
 
 class Curve {
 	public:
 		Curve();
-		Curve(Line, t);
+		Curve(vector<Point> points, float d);
 		Curve(const Curve& c);
 		Curve& operator=(const Curve& c);
 		~Curve();
 
-		Line line;
-		int t;
+		void quadraticBezier(float t)
+
+		vector<Point> points;
+		vector<Point> finals;
+		Point pFinal;
+		float d;
 		
 };
 
