1e8ed5130c1e81392ef2b4740a12d5974ec5f756
vanydeasy <vanyadeasysafrina@yahoo.co.id>
2016-03-06 22:23:53 +0700

    Main DONE | Bug found in Face class

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include <termios.h>
 #include <fstream>
 #include "assets.h"
+#include "Face.h"
 
 using namespace std;
 
 FrameBuffer FB;
 bool quit = false;
 int key;
-Face myFace;
 
 int kbhit(void);
 vector<Point> matrixToPolygon(int object[][2], int col);
@@ -17,7 +17,7 @@ void colorFace();
 
 int main() {
 	
-	// INIT
+	// --------------INIT-------------- //
 	vector<Point> face_vector =  matrixToPolygon(v_face,sizeof(v_face)/sizeof(*v_face));
 	vector<Point> left_eyebrow_vector =  matrixToPolygon(v_left_eyebrow,sizeof(v_left_eyebrow)/sizeof(*v_left_eyebrow));
 	vector<Point> right_eyebrow_vector =  matrixToPolygon(v_right_eyebrow,sizeof(v_right_eyebrow)/sizeof(*v_right_eyebrow));
@@ -28,72 +28,85 @@ int main() {
 	vector<Point> nose_vector = matrixToPolygon(nose, sizeof(nose)/sizeof(*nose));
 	vector<Point> upperLipVector = matrixToPolygon(upper_lip, sizeof(upper_lip)/sizeof(*upper_lip));
 	vector<Point> bottomLipVector = matrixToPolygon(bottom_lip, sizeof(bottom_lip)/sizeof(*bottom_lip));
-
-	system("clear");
+	vector<Point> eyeTagVector = matrixToPolygon(eye_tag, sizeof(eye_tag)/sizeof(*eye_tag));
+	vector<Point> lipTagVector = matrixToPolygon(lip_tag, sizeof(lip_tag)/sizeof(*lip_tag));
+	vector<Point> mouthTagVector = matrixToPolygon(mouth_tag, sizeof(mouth_tag)/sizeof(*mouth_tag));
 
 	map<string, vector<Point> > faceMap;
 	
-	faceMap.insert("head",face_vector);
-	faceMap.insert("nose",nose_vector);
-	faceMap.insert("right_eyebrow",right_eyebrow_vector);
-	faceMap.insert("left_eyebrow",left_eyebrow_vector);
-	faceMap.insert("right_upper_eyelid",right_upper_eye_vector);
-	faceMap.insert("right_lower_eyelid",right_lower_eye_vector);
-	faceMap.insert("left_upper_eyelid",left_upper_eye_vector);
-	faceMap.insert("left_lower_eyelid",left_lower_eye_vector);
-	faceMap.insert("upper_lip",upperLipVector);
-	faceMap.insert("lower_lip",bottomLipVector);
+	faceMap.insert(std::pair<std::string, vector<Point> >("head",face_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("nose",nose_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("right_eyebrow",right_eyebrow_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("left_eyebrow",left_eyebrow_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("right_upper_eyelid",right_upper_eye_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("right_lower_eyelid",right_lower_eye_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("left_upper_eyelid",left_upper_eye_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("left_lower_eyelid",left_lower_eye_vector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("upper_lip",upperLipVector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("lower_lip",bottomLipVector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("lip_tag",lipTagVector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("laugh_tag",mouthTagVector));
+	faceMap.insert(std::pair<std::string, vector<Point> >("eye_tag",eyeTagVector));
 	
-	myFace(faceMap);
 
-	// END OF INIT
+	printf("masuk sini sebelum?\n");
+	// Face myFace(faceMap); // >>>>> NGEBUG COOOY <<<<< //
+	printf("masuk sini sesudah?\n");
+
+	// --------------END OF INIT-------------- //
+
+	system("clear");
 	FB.clearscreen();
 
+
 	while(!quit){
-		Curve face(face_vector,0.05);
-		Curve left_eyebrow(left_eyebrow_vector,0.05);
-		Curve right_eyebrow(right_eyebrow_vector,0.05);
-		Curve left_upper_eye(left_upper_eye_vector,0.05);
-		Curve right_upper_eye(right_upper_eye_vector,0.05);
-		Curve left_lower_eye(left_lower_eye_vector,0.05);
-		Curve right_lower_eye(right_lower_eye_vector,0.05);
-		Curve noseCurve(noseVector, 0.05);
-		Curve upperLipCurve(upperLipVector, 0.05);
-		Curve bottomLipCurve(bottomLipVector, 0.05);
-		
 		if(kbhit()){
 			key=getchar();
 			FB.cleararea(250,90,550,400);
 			//PANGGIL FUNGSI UNTUK REDRAW MOVEMENT
 			if(key=='a' || key=='A') {
 				// SMILE
-				myFace.smile(1);
+				// myFace.smile(1);
 				
 				FB.drawCircle(Point(345,248),9,255, 255, 255, 0);
 				FB.drawCircle(Point(455,248),9,255, 255, 255, 0);
 			}
 			else if(key=='s' || key=='S') {
-				// SAD
-				myFace.sad(1);
+				// LAUGH
+				// myFace.laugh(1);
 
 				FB.drawCircle(Point(345,248),9,255, 255, 255, 0);
 				FB.drawCircle(Point(455,248),9,255, 255, 255, 0);
 			}
 			else if(key=='d' || key=='D') {
 				// CRY
-				myFace.cry(1,1);
+				// myFace.cry(1,1);
 
 				FB.drawCircle(Point(345,248),9,255, 255, 255, 0);
 				FB.drawCircle(Point(455,248),9,255, 255, 255, 0);
 			}
 			else if(key=='f' || key=='F') {
 				// MEREM
-				myFace.eyeclosed();
+				// myFace.eyesClosed();
 			}
 			else if(key=='q') {
 				quit = true;
 			}
-
+			else {
+				FB.drawCircle(Point(345,248),9,255, 255, 255, 0);
+				FB.drawCircle(Point(455,248),9,255, 255, 255, 0);
+			}
+			Curve face(face_vector,0.05);
+			Curve left_eyebrow(left_eyebrow_vector,0.05);
+			Curve right_eyebrow(right_eyebrow_vector,0.05);
+			Curve left_upper_eye(left_upper_eye_vector,0.05);
+			Curve right_upper_eye(right_upper_eye_vector,0.05);
+			Curve left_lower_eye(left_lower_eye_vector,0.05);
+			Curve right_lower_eye(right_lower_eye_vector,0.05);
+			Curve noseCurve(nose_vector, 0.05);
+			Curve upperLipCurve(upperLipVector, 0.05);
+			Curve bottomLipCurve(bottomLipVector, 0.05);
+			
 			FB.drawCurve(face, 255, 255, 255, 0);
 			FB.drawCurve(left_eyebrow, 0, 0, 0, 0);
 			FB.drawCurve(right_eyebrow, 0, 0, 0, 0);
